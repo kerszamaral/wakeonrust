128e319556a75699bc51a74fdcc0810730857f41
kerszamaral <kerszamaral@gmail.com>
2024-04-26 16:24:42 -0300

    Made discovery send right pakcets

--- src/subservices/discovery.rs
+++ src/subservices/discovery.rs
@@ -1,40 +1,57 @@
-use crate::pcinfo;
-use crate::pcinfo::PCInfo;
+use self::find::find_manager;
+use self::listen::listen_for_clients;
+use crate::packets::{
+    check_packet, make_header, swap_packet_type, BUFFER_SIZE, HEADER_SIZE, SSR_ACK_PACKET,
+    SSR_PACKET,
+};
+use crate::pcinfo::{PCInfo, PCStatus};
 use crate::{
     delays::{CHECK_DELAY, WAIT_DELAY},
     ports::DISCOVERY_PORT,
     signals::Signals,
 };
 use mac_address::MacAddress;
-use std::net::{Ipv4Addr, SocketAddr, UdpSocket};
-use std::str::FromStr;
+use std::net::{SocketAddr, UdpSocket};
 use std::sync::{atomic::Ordering, mpsc::Sender};
 
-use self::find::find_manager;
-use self::listen::listen_for_clients;
+fn from_buffer(buf: &[u8], amt: usize) -> Option<(String, MacAddress)> {
+    let msg = &buf[..amt];
+    if !check_packet(&msg.to_vec(), SSR_PACKET) {
+        return None;
+    }
+    let msg = &buf[HEADER_SIZE..amt];
+
+    const MAC_SIZE: usize = 6;
+    let hostname_bytes = &msg[MAC_SIZE..];
+    let mac_bytes = &msg[..MAC_SIZE];
+
+    let hostname = String::from_utf8(hostname_bytes.to_vec()).unwrap_or("".to_string());
+    let mac = MacAddress::new(mac_bytes.try_into().unwrap_or([0 as u8; MAC_SIZE]));
+    return Some((hostname, mac));
+}
 
 mod find {
     use super::*;
 
-    pub fn find_manager(socket: &UdpSocket, signals: &Signals, new_pc_tx: &Sender<PCInfo>) -> bool {
+    pub fn find_manager(signals: &Signals, socket: &UdpSocket, new_pc_tx: &Sender<PCInfo>) -> bool {
         while signals.run.load(Ordering::Relaxed) {
-            let mut buf = [0; 1024];
+            let mut buf = [0; BUFFER_SIZE];
             match socket.recv_from(&mut buf) {
-                Ok((_amt, src)) => {
-                    // let msg = std::str::from_utf8(&buf[..amt]).unwrap();
-                    // if msg == "MANAGER" {
-                    //     return true;
-                    // }
-                    let hostname = "pc1".to_string();
-                    let mac = MacAddress::new([0, 0, 0, 0, 0, 1]);
-                    let ip =
-                        Ipv4Addr::from_str(src.ip().to_canonical().to_string().as_str()).unwrap();
+                Ok((amt, src)) => {
+                    let (hostname, mac) = match from_buffer(&buf, amt) {
+                        Some((hostname, mac)) => (hostname, mac),
+                        None => return false,
+                    };
+
+                    let ip = src.ip().to_canonical();
                     let new_manager =
-                        PCInfo::new(hostname, mac, ip, pcinfo::PCStatus::Online, true);
+                        PCInfo::new(hostname, mac, ip, PCStatus::Online, true);
                     new_pc_tx.send(new_manager).unwrap();
                     return true;
                 }
-                Err(_) => {}
+                Err(_) => {
+                    return false;
+                }
             }
         }
         false
@@ -44,20 +61,19 @@ mod find {
 mod listen {
     use super::*;
 
-    pub fn listen_for_clients(socket: &UdpSocket, new_pc_tx: &Sender<PCInfo>) {
-        let mut buf = [0; 1024];
+    pub fn listen_for_clients(socket: &UdpSocket, new_pc_tx: &Sender<PCInfo>, ssra: &Vec<u8>) {
+        let mut buf = [0; BUFFER_SIZE];
         match socket.recv_from(&mut buf) {
-            Ok((_amt, src)) => {
-                // let msg = std::str::from_utf8(&buf[..amt]).unwrap();
-                // if msg == "MANAGER" {
-                //     return true;
-                // }
-                let hostname = "pc1".to_string();
-                let mac = MacAddress::new([0, 0, 0, 0, 0, 1]);
-                let ip = Ipv4Addr::from_str(src.ip().to_canonical().to_string().as_str()).unwrap();
-                let new_client = PCInfo::new(hostname, mac, ip, pcinfo::PCStatus::Online, false);
+            Ok((amt, src)) => {
+                let (hostname, mac) = match from_buffer(&buf, amt) {
+                    Some((hostname, mac)) => (hostname, mac),
+                    None => return,
+                };
+                let ip = src.ip().to_canonical();
+                let new_client = PCInfo::new(hostname, mac, ip, PCStatus::Online, false);
+
                 new_pc_tx.send(new_client).unwrap();
-                socket.send_to("MANAGER".as_bytes(), src).unwrap();
+                socket.send_to(&ssra, src).unwrap();
             }
             Err(_) => {}
         }
@@ -65,30 +81,44 @@ mod listen {
 }
 
 pub fn initialize(signals: &Signals, new_pc_tx: &Sender<PCInfo>) {
+    // Setup the socket
     let addr = SocketAddr::from(([127, 0, 0, 1], DISCOVERY_PORT));
     let broadcast_addr = SocketAddr::from(([255, 255, 255, 255], DISCOVERY_PORT));
     let socket = UdpSocket::bind(addr).expect("Failed to bind monitor socket");
     socket
         .set_nonblocking(true)
-        .expect("Failed to set non-blocking mode");
+        .expect("Failed to set discovery socket to non-blocking mode");
     socket
         .set_read_timeout(Some(CHECK_DELAY))
-        .expect("Failed to set read timeout");
+        .expect("Failed to set discovery socket read timeout");
+
+    // Setup the SSR packet
+    let our_mac = mac_address::get_mac_address()
+        .unwrap()
+        .expect("Failed to get MAC address");
+    let our_name = gethostname::gethostname().into_string().unwrap();
+    let length = our_mac.bytes().len() + our_name.as_bytes().len();
+
+    // Make the SSR packet and its ACK
+    let ssr = [
+        make_header(SSR_PACKET, length).to_vec(),
+        our_mac.bytes().to_vec(),
+        our_name.as_bytes().to_vec(),
+    ]
+    .concat();
+    let ssra = swap_packet_type(&ssr, SSR_ACK_PACKET);
 
     while signals.run.load(Ordering::Relaxed) {
         if signals.is_manager.load(Ordering::Relaxed) {
-            listen_for_clients(&socket, &new_pc_tx);
+            listen_for_clients(&socket, &new_pc_tx, &ssra);
         } else {
-            let manager_found = find_manager(&socket, &signals, &new_pc_tx);
+            let manager_found = find_manager(&signals, &socket, &new_pc_tx);
 
             if manager_found {
-                signals.is_manager.store(true, Ordering::Relaxed);
                 signals.manager_found.store(true, Ordering::Relaxed);
             } else {
                 socket.set_broadcast(true).unwrap();
-                socket
-                    .send_to("MANAGER".as_bytes(), broadcast_addr)
-                    .unwrap();
+                socket.send_to(&ssr, broadcast_addr).unwrap();
                 socket.set_broadcast(false).unwrap();
             }
 
