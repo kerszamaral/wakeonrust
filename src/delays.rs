28faa80883ce05ee9b60a9ebf52b8f3de76ee06b
kerszamaral <kerszamaral@gmail.com>
2024-04-29 18:25:26 -0300

    Networkg reconfiguration now working

--- src/delays.rs
+++ src/delays.rs
@@ -4,4 +4,4 @@ use std::time::Duration;
 pub const INPUT_DELAY: Duration = Duration::from_millis(50);
 pub const WAIT_DELAY: Duration = Duration::from_millis(100);
 pub const CHECK_DELAY: Duration = Duration::from_millis(100);
-pub const ELECTION_FINISHED_DELAY: Duration = Duration::from_millis(50);
+pub const ELECTION_DELAY: Duration = Duration::from_millis(50);
